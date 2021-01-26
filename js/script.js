95fc25d50f7fa0724561218731a1554e2c92edb2
Gennaro <scognamiglioge@hotmail.it>
2021-01-26 16:26:24 +0100

    Inizio e lo continuo più tardi

---
+++ js/script.js
@@ -0,0 +1,23 @@
+let app = new Vue({
+    el: '#format',
+    data:{
+        immagini : [
+            '',
+            '',
+            '',
+            ''
+        ]
+    },
+
+    methods:{
+        
+    }
+
+
+
+
+
+
+
+
+});
