ff270f1a896349016be9fd7857e4faed3737fb66
Gennaro <scognamiglioge@hotmail.it>
2021-01-27 16:16:57 +0100

    Ora faccio il bonus

--- js/script.js
+++ js/script.js
@@ -1,23 +1,23 @@
-let app = new Vue({
+let format = new Vue({
     el: '#format',
     data:{
-        immagini : [
-            '',
-            '',
-            '',
-            ''
+        contatore: 0,
+        foto : [
+            'https://cdn.pixabay.com/photo/2021/01/20/09/29/islet-5933589__340.jpg',
+            'https://cdn.pixabay.com/photo/2021/01/20/07/45/soap-bubble-5933332__340.jpg',
+            'https://cdn.pixabay.com/photo/2021/01/19/09/09/sunset-5930547__340.jpg',
+            'https://cdn.pixabay.com/photo/2021/01/20/09/29/landscape-5933588__340.jpg'
         ]
     },
-
     methods:{
-        
-    }
+        // funzione per scorrere in avanti
+        avanti(){
+        (this.contatore ==(this.foto.length - 1 )) ? this.contatore = 0 : this.contatore++;
+        },
 
-
-
-
-
-
-
-
+        // funzione per scorrere indietro
+        indietro(){
+        (this.contatore < 1) ? this.contatore = (this.foto.length-1) : this.contatore--;
+        }
+    }   
 });
